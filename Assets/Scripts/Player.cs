8ce6ba8e9f016bf0d1c304650e1852967698d771
hectorborges <hectorborges94@gmail.com>
2020-03-17 15:51:26 -0500

    Initial Commit
    *Added basic player movement
    *Added basic networking
    *Added TMPro

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Player : MonoBehaviourPun, IPunObservable
+{
+    public float speed = 5;
+    public float rotationSpeed = 100;
+    public bool hideCursor = true;
+
+    Camera cam;
+
+    float horizontal;
+    float vertical;
+    float mouseX;
+    float mouseY;
+
+    private void Awake()
+    {
+        if (!photonView.IsMine)
+            enabled = false;
+    }
+
+    private void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+
+        if(hideCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        move = transform.TransformDirection(move);
+        move.y = 0;
+        transform.position += move.normalized * speed * Time.deltaTime;
+    }
+
+    void Rotate()
+    {
+
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY += Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(0, mouseX, 0);
+        cam.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0);
+    }
+
+    public static void RefereshInstance(ref Player player, Player prefab)
+    {
+        Vector3 position = Vector3.zero;
+        Quaternion rotation = Quaternion.identity;
+
+        if (player != null)
+        {
+            position = player.transform.position;
+            rotation = player.transform.rotation;
+            PhotonNetwork.Destroy(player.gameObject);
+        }
+
+        player = PhotonNetwork.Instantiate(prefab.gameObject.name, position, rotation).GetComponent<Player>();
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(horizontal);
+            stream.SendNext(vertical);
+            stream.SendNext(mouseX);
+            stream.SendNext(mouseY);
+        }
+        else
+        {
+            horizontal = (float)stream.ReceiveNext();
+            vertical = (float)stream.ReceiveNext();
+            mouseX = (float)stream.ReceiveNext();
+            mouseY = (float)stream.ReceiveNext();
+        }
+    }
+}
