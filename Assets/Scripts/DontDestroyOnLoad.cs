284de6ee9dfa03e154ddb660c50de3d3e1c372fd
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-03-26 14:07:57 -0500

    added instance check

--- Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    public static GameObject Instance;
     private void Start()
     {
         DontDestroyOnLoad(this);
+
+        if (Instance == null)
+            Instance = gameObject;
+        else
+            Destroy(gameObject);
     }
 }
