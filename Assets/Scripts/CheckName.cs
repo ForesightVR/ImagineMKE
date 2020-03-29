76076f6ddc278a23663a491f74ab2afcefc8a35d
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-03-29 14:50:14 -0500

    Added a name requirement, changed some UI to be scalable

---
+++ Assets/Scripts/CheckName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CheckName : MonoBehaviour
+{
+    public GameObject nameWarning;
+
+    public void Enter(TMP_InputField inputField)
+    {
+        if (inputField.text == "")
+            nameWarning.SetActive(true);
+        else
+            NetworkConnectionManager.Instance.ConnectToMaster();
+    }
+}
