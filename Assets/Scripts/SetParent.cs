1b6a00f46bd8270722adddbbe44bedacd9d750c9
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-04-08 14:56:41 -0500

    added salsa

---
+++ Assets/Scripts/SetParent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetParent : MonoBehaviour
+{
+    [InspectorButton("SetUp")]
+    public bool setUp;
+
+    public string childName = "CC_Game_Eye";
+    public string parentName = "CC_Base_Head";
+
+    void SetUp()
+    {
+        GameObject child = null;
+        GameObject parent = null;
+
+        List<Transform> children = new List<Transform>();
+
+        GetAllChildren(transform, ref children);
+
+        foreach(Transform t in children)
+        {
+            if (t.name == childName)
+                child = t.gameObject;
+            else if (t.name == parentName)
+                parent = t.gameObject;
+        }
+
+        if (child && parent)
+        {
+            child.transform.parent = parent.transform;
+            DestroyImmediate(this);
+        }
+        else
+        {
+            Debug.Log("Could not find parent or child!");
+        }
+    }
+
+    public static void GetAllChildren(Transform parent, ref List<Transform> transforms)
+    {
+
+        foreach (Transform t in parent)
+        {
+
+            transforms.Add(t);
+
+            GetAllChildren(t, ref transforms);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Shared Scripts/Utilities/SetUpEyes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SetUpEyes : MonoBehaviour
+{
+    [InspectorButton("SetUp")]
+    public bool setUp;
+    SkinnedMeshRenderer skin;
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
+
+    private void SetUp()
+    {
+        if (!skin)
+            skin = GetComponent<SkinnedMeshRenderer>();
+
+        if (!meshRenderer)
+        {
+            meshRenderer = GetComponent<MeshRenderer>();
+
+            if (!meshRenderer)
+                meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+
+        if (!meshFilter)
+        {
+            meshFilter = GetComponent<MeshFilter>();
+
+            if (!meshFilter)
+                meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+
+        if (skin && meshRenderer && meshFilter)
+        {
+            meshFilter.mesh = skin.sharedMesh;
+            meshRenderer.material = skin.materials[0];
+
+            DestroyImmediate(skin);
+            DestroyImmediate(this);
+        }
+    }
+}
