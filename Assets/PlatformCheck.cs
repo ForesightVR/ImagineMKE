aa10fe2994e3342ce6c89199b060a5b4fba7c495
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-04-10 10:25:17 -0500

    added platform check

---
+++ Assets/PlatformCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class PlatformCheck : MonoBehaviour
+{
+#if UNITY_STANDALONE_OSX
+    IEnumerator Start()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+
+        if (Application.HasUserAuthorization(UserAuthorization.Microphone))
+            Debug.Log("Microphone Found");
+        else
+            Debug.Log("Could Not Find Microphone!");
+    }
+#endif
+}
