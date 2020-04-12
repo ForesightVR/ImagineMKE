a82be8705c3b322ec24ec890dcbdbf6a660e9669
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-04-11 20:20:24 -0500

    added sound manager

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Michsky.UI.ModernUIPack;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public GameObject volumeCanvas;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            volumeCanvas.SetActive(!volumeCanvas.activeInHierarchy);
+    }
+
+    public void ChangeMasterVolume(SliderManager sliderManager)
+    {
+        ChangeVolume("MasterVolume", sliderManager.mainSlider.value);
+    }
+
+    public void ChangeMusicVolume(SliderManager sliderManager)
+    {
+        ChangeVolume("MusicVolume", sliderManager.mainSlider.value);
+    }
+
+    public void ChangeEnvironmentVolume(SliderManager sliderManager)
+    {
+        ChangeVolume("EnvironmentVolume", sliderManager.mainSlider.value);
+    }
+
+    public void ChangeVoiceChatVolume(SliderManager sliderManager)
+    {
+        ChangeVolume("VoiceChatVolume", sliderManager.mainSlider.value);
+    }
+
+    void ChangeVolume(string group, float volume)
+    {
+        mixer.SetFloat(group, volume);
+    }
+}
