a86a899416ab4c8c01c9bb2fdd83ca0e9e16e363
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-04-14 12:03:16 -0500

    fix for first person clipping

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Foresight;
+using System.Linq;
 
 public class CameraControl : MonoBehaviour
 {
@@ -18,6 +19,8 @@ public class CameraControl : MonoBehaviour
     Vector3 velocity;
     Transform player;
 
+    public List<Renderer> renderers = new List<Renderer>();
+
     private void Start()
     {
         player = Player.LocalPlayerInstance.transform;
@@ -27,15 +30,32 @@ public class CameraControl : MonoBehaviour
     {
         //character.SetActive(thirdPersonActive ? true : false);
 
-        Player.LocalPlayerInstance.characterSelected.layer = thirdPersonActive ? LayerMask.NameToLayer("Default") : LayerMask.NameToLayer("FirstPerson");
+        if(renderers.Count <= 0)
+            renderers = Player.LocalPlayerInstance.characterSelected.GetComponentsInChildren<Renderer>().ToList();
+
+        if (renderers.Count <= 0) return;
 
         if (thirdPersonActive)
         {
+            if(renderers[0].gameObject.layer == LayerMask.NameToLayer("FirstPerson"))
+            {
+                foreach (Renderer rend in renderers)
+                    rend.gameObject.layer = LayerMask.NameToLayer("Default");
+            }
+
             thirdPerson = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * thirdPerson;
             transform.position = Vector3.SmoothDamp(transform.position, player.position + thirdPerson, ref velocity,  transitionSpeed);
             transform.LookAt(player.position + Vector3.up * lookAtHeight);
         }
         else
+        {
+            if (renderers[0].gameObject.layer == LayerMask.NameToLayer("Default"))
+            {
+                foreach (Renderer rend in renderers)
+                    rend.gameObject.layer = LayerMask.NameToLayer("FirstPerson");
+            }
+
             transform.position = Vector3.SmoothDamp(transform.position, player.position + firstPerson, ref velocity, transitionSpeed);
+        }
     }
 }
