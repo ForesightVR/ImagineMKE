9a904f479b4b7b066fb994e2f60f6623f707d3cb
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-04-15 13:58:55 -0500

    hand adding pics

---
+++ Assets/ArtistDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ArtistDatabase")]
+public class ArtistDatabase : ScriptableObject
+{
+    [System.Serializable]
+    public struct ArtData
+    {
+        public Sprite artImage;
+        public int productID;
+    }
+
+    [System.Serializable]
+    public struct ArtistData
+    {
+        public string aristName;
+        public ArtData[] artData;
+    }
+
+    public ArtistData[] artistData;
+}
